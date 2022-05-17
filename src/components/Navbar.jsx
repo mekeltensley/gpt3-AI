f346bd52b388c84f382a65f1bc98536bc0804b00
Mekel Tensley <mekelintech@gmail.com>
2022-05-17 15:19:45 -0400

    Initial commit, create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+class Navbar extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default Navbar;
